a5ff1c62307070266063a4fa71795c8b9756289a
issa23i <isabelpastorlopezcv@gmail.com>
2026-01-10 20:59:06 +0100

    feat: ejercicio 16

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Inicio curso RXJS");
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<string> = {
+  next: (value) => console.log("siguiente [next]: ", value),
+  error: (err) => console.warn("error [observer]: ", err),
+  complete: () => console.info("completado [observer] "),
+};
+
+const obsservable$ = new Observable<string>((subscriber) => {
+  subscriber.next("Hola");
+  subscriber.next("Mundo");
+
+  // forzar un error
+  const a = undefined;
+  a.nombre = "Isa";
+
+  subscriber.complete();
+
+  subscriber.next("Adiós");
+});
+
+obsservable$.subscribe(observer);
+
+// obsservable$.subscribe(
+//   (valor) => console.log("next: ", valor),
+//   (error) => console.warn("error: ", error),
+//   () => console.info("complete")
+// );
