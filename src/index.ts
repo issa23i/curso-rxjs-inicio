1d1e7fa2b2bfb1ce4995de8a9d397a5dfdf1b13b
issa23i <isabelpastorlopezcv@gmail.com>
2026-01-10 21:25:31 +0100

    feat: clase 17

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,29 @@ const observer: Observer<string> = {
   error: (err) => console.warn("error [observer]: ", err),
   complete: () => console.info("completado [observer] "),
 };
+
+const intervalo$ = new Observable<number>((subscriber) => {
+  // crear un contador
+  let uno = 1;
+  const interval = setInterval(() => {
+    uno++;
+    subscriber.next(uno);
+    console.log(uno);
+  }, 3500);
+
+  return () => {
+    clearInterval(interval);
+    console.log("intervalo destruido");
+  };
+});
+
+const subcription1 = intervalo$.subscribe();
+const subcription2 = intervalo$.subscribe();
+const subcription3 = intervalo$.subscribe();
+
+setTimeout(() => {
+  subcription1.unsubscribe();
+  subcription2.unsubscribe();
+  subcription3.unsubscribe();
+  console.log("completado timeout");
+}, 10500);
