ab25ce0da6f23185fd871480bb7785fc86656722
issa23i <isabelpastorlopezcv@gmail.com>
2026-01-10 21:02:09 +0100

    feat: video 16

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,3 @@ const observer: Observer<string> = {
   error: (err) => console.warn("error [observer]: ", err),
   complete: () => console.info("completado [observer] "),
 };
-
-const obsservable$ = new Observable<string>((subscriber) => {
-  subscriber.next("Hola");
-  subscriber.next("Mundo");
-
-  // forzar un error
-  const a = undefined;
-  a.nombre = "Isa";
-
-  subscriber.complete();
-
-  subscriber.next("Adiós");
-});
-
-obsservable$.subscribe(observer);
-
-// obsservable$.subscribe(
-//   (valor) => console.log("next: ", valor),
-//   (error) => console.warn("error: ", error),
-//   () => console.info("complete")
-// );
